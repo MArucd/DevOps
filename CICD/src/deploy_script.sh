f0c04b9190e76b8ce309d3df367b9b4a31b45a54
lifeboal <lifeboal>
2025-08-19 18:14:43 +0300

    s21

--- CICD/src/deploy_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-scp cat/s21_cat lifeboal@192.168.100.11:/usr/local/bin/
-scp grep/s21_grep lifeboal@192.168.100.11:/usr/local/bin/
-
-ssh lifeboal@192.168.100.11 ls -lah /usr/local/bin/
